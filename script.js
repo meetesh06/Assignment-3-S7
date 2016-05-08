cd616baa7d2272a6c7b19c29cbf97e37ea34ca3d
meetesh06 <meeteshmehta4@gmail.com>
2016-05-08 21:04:18 +0530

    Fix bugs

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var input = document.getElementById('text_input');
 var view = document.getElementById('viewer');
-var switcher = false;
+var switcher = false; //trigger alarm, when its true
 function printtext () {
-	var i_val = input.value.split('');
-	console.log(i_val);
-	for (var i = 0; i < i_val.length; i++) {
-		switch(i_val[i]) {
+	var i_val = input.value.split(''); //get the text value
+	for (var i = 0; i < i_val.length; i++) { //check all the values
+		switch(i_val[i]) { //if switcher finds a letter or something else, trigger's off the switcher
 			case "0":
 			case "1":
 			case "2":
@@ -16,28 +15,21 @@ function printtext () {
 			case "7":
 			case "8":
 			case "9":
-				console.log("RJ was a number here");
-				switcher = false;
 				break;
 			default: 
-				console.log("NON NUMBER");
 				switcher = true;
 				break;
-
 		}
+	} 
+	if (switcher === false) { //only triggered when all the element's above are sweet
+		view.innerHTML = input.value; // set the HTML
+		view.style.background = "#39568D"; //default background
+	} else {//when switcher is true, change the background color and set the trip alarm again
+		view.style.background = "#FF4C57"; // red background
+		switcher = false; // retrigger alarm
 	}
-	if (switcher === false) {
-		view.innerHTML = input.value;
-		view.style.background = "#39568D";
-	} else {
-		view.style.background = "red";
-	}
- 	
-	
 }
-
 input.addEventListener("input", printtext);
-
 // var input = document.getElementById('text_input');
 // var view = document.getElementById('viewer');
 // function switcher (a) {
